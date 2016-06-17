defdc1fc5519c022a0efafdd05223e39385b1fb0
Regi Babamusta <babamusta@optonline.net>
2016-06-17 13:52:42 -0400

    first commit

---
+++ javacheat1.js
@@ -0,0 +1,123 @@
+	
+// 	// Variables
+// 	var a = 8;
+// 	var b = 9;
+// 	var c = a + b;
+
+
+// 	// Debugging (alerts, comments, the console)
+// 	a = 7;
+// 	b = 8;
+// 	c = a + b;
+// 	console.log(c)
+
+
+// 	// different data types
+// 	// string - "Hello World"
+// 	// Number - 5, 5.5, 1000 (all number in JS are floats)
+// 	// Boolean - true, false
+// 	// Undefined - (no value)
+
+
+// 	// arrays
+// 	//Strings 
+// 	cars = ["BMW", "Mercedes", "Audi", "Porsche"];
+// 	//Multiple 
+// 	hi = [35, 20, 55, "umbrella"];
+// 	//Variables var car_names = [
+// 	othercars = ["Nissan", "Infiniti", "honda"];
+
+
+// 	// testing
+// 	24 > 56;
+// 	//> false
+
+
+// 	// logic
+
+
+// 	// functions
+// 	function someName(numberOne){
+// 		return (numberOne + 10);
+// }
+
+	/* 2. Write 3 different functions that take input and return something via console,
+	or an alert.*/
+
+	//function addthirteen(number){
+    //return number + 13; 
+	//}
+	
+	//console.log(addthirteen(15));
+
+	//function isEven(value) {
+    //return (value%2 == 0);
+	//}
+	//console.log(isEven(25));
+
+	//function dividebyfive(thing){
+    //return (thing/5);
+	//}
+	//console.log(dividebyfive(25))
+
+
+
+	/* 1.Create a script with two variables assigned to two numbers. Add them together 
+	and output the result to the console. Now do the same thing with 2 strings. */
+	
+	// var a = 4;
+	// var b = 5;
+	// console.log(a + b)
+
+	// var a = "bob ";
+	// var b = "johnson";
+	// console.log(a + b)
+
+	/* 2. Create a multidimensional array related to a subject that interests you. Output
+	two of the items in the sub-arrays to the console. */
+
+	//strings
+// function myFunction() {
+// 		var ferrari = ["458italia", "laferrari"];
+// 		var lamborghini = ["huracan", "gallardo"]
+// 		var cars = [ferrari, lamborghini]
+// 		console.log(ferrari + lamborghini)
+// 		}
+
+
+	/* 3.Write a script that checks if a variable is less than 10. If it is, 
+	alert the user that their variable is less than 10. If it is not, using the console,
+	let the user know that what the variable was and that it was greater than 10. */
+
+		function lessthan10(num){
+			if(num<10){
+				 alert("Your variable was less than 10.")
+			}else{
+				 console.log("Your number was" + num + "and was greater than 10.")
+			}
+		}	
+		lessthan10(12)
+
+	/* 4. Try running the script and then changing the variable's value to see how
+	this affects the program's output.*/
+
+		lessthan10(7vb)
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
